d4abd292e7840a0119a15cd20e4ccd7aa3ffd9f8
Arun <pratikarun@gmail.com>
2011-04-03 23:50:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pomodoro extension for gnome-shell
+- Provides a countdown timer in the gnome-shell top panel
+- Keeps track of completed 25 minute cycles
+
+More on pomodoro technique [here](http://www.pomodorotechnique.com).
+
+# Installation
+- Copy the files in *src* to *~/.local/share/gnome-shell/extensions/pomodoro@arun.codito.in* directory
+- Press *Alt + F2*, and *r* in command to restart gnome-shell
+- Click on the panel item to toggle the timer state
+
+# Bugs
+I can be reached at arun at codito dot in.
+
+# Todo
+- Add global key handlers to start/stop timer
+- Timer should stop when system is locked
+- Add dialog box to show up for 5 minute rest durations
+- Integrate timer to Getting Things Gnome task list
---
+++ src/extension.js
@@ -0,0 +1,100 @@
+// Sample extension code, makes clicking on the panel show a message
+const Clutter = imports.gi.Clutter;
+const DBus = imports.dbus;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Shell = imports.gi.Shell;
+const Gvc = imports.gi.Gvc;
+const Signals = imports.signals;
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Panel = imports.ui.panel;
+const ModalDialog = imports.ui.modalDialog;
+
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Util = imports.misc.util;
+
+const Gettext = imports.gettext.domain('gnome-shell');
+const _ = Gettext.gettext;
+
+let _pomodoroInit = false;
+
+function Indicator() {
+    this._init.apply(this, arguments);
+}
+
+Indicator.prototype = {
+    __proto__: PanelMenu.SystemStatusButton.prototype,
+
+    _init: function() {
+        PanelMenu.SystemStatusButton.prototype._init.call(this, 'text-x-generic-symbol');
+
+        this._timer = new St.Label();
+        this._timeSpent = -1;
+        this._stopTimer = true;
+        this._sessionCount = 0;
+
+        this._timer.set_text("[0] --:--");
+        this.actor.add_actor(this._timer);
+
+        // Toggle timer state button
+        let widget = new PopupMenu.PopupSwitchMenuItem(_("Toggle timer"), false);
+        widget.connect("toggled", Lang.bind(this, this._toggleTimerState));
+        this.menu.addMenuItem(widget);
+
+        // Register keybindings to toggle
+        //let shellwm = global.window_manager;
+        //shellwm.takeover_keybinding('something_new');
+        //shellwm.connect('keybinding::something_new', function () {
+            //Main.runDialog.open();
+        //});
+
+        // Bind to system events - like lock or away
+
+        // Start the timer
+        this._refreshTimer();
+    },
+
+    _toggleTimerState: function(item) {
+        this._stopTimer = item.state;
+        if (this._stopTimer == false) {
+            this._stopTimer = true;
+            this._timer.set_text("[" + this._sessionCount + "] --:--");
+        }
+        else {
+            this._timeSpent = -1;
+            this._stopTimer = false;
+            this._refreshTimer();
+        }
+    },
+
+    _refreshTimer: function() {
+        if (this._stopTimer == false) {
+            this._timeSpent += 1;
+            if (this._timeSpent > 25) {
+                this._timeSpent = 0;
+                this._sessionCount += 1;
+            }
+
+            if (this._timeSpent < 10)
+                this._timer.set_text("[" + this._sessionCount + "] 00:0" + this._timeSpent.toString());
+            else
+                this._timer.set_text("[" + this._sessionCount + "] 00:" + this._timeSpent.toString());
+
+            Mainloop.timeout_add_seconds(60, Lang.bind(this, this._refreshTimer));
+        }
+
+        return false;
+    }
+};
+
+// Put your extension initialization code here
+function main() {
+    if (!_pomodoroInit) {
+        Main.StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['pomodoro'] = 'pomodoro';
+        Main.Panel.STANDARD_TRAY_ICON_ORDER.unshift('pomodoro');
+        Main.Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['pomodoro'] = Indicator;
+        _pomodoroInit = true;
+    }
+}
