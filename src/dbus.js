048c6dab3a80446176fe05c8b04275be5fa7340e
Slawomir Dawidziuk <sdawid@gmx.com>
2012-02-16 23:42:48 +0100

    Unexport dbus object when disabling extension

--- src/dbus.js
+++ src/dbus.js
@@ -139,6 +139,10 @@ PomodoroTimer.prototype = {
         DBus.session.emit_signal('/org/gnome/Shell/Extensions/Pomodoro',
                                  'org.gnome.Shell.Extensions.Pomodoro',
                                  'NotifyPomodoroEnd', '', []);
+    },
+
+    destroy: function() {
+        DBus.session.unexportObject(this);
     }
 };
 
