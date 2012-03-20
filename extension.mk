e6e379c26d2d8b112d4b1bd8b7d3120778e8f578
Arun <pratikarun@gmail.com>
2012-03-20 22:35:18 +0530

    Added utilities for translation and settings. Added support for using locales and schemas from local install directory.

--- extension.mk
+++ extension.mk
@@ -5,4 +5,4 @@ topextensiondir = $(datadir)/gnome-shell/extensions
 extensiondir = $(topextensiondir)/$(uuid)
 
 gschemas_in = org.gnome.shell.extensions.pomodoro.gschema.xml.in
-gschemaname = $(gschemas_in:.xml.in=.xml)
+gschemaname = org.gnome.shell.extensions.pomodoro
