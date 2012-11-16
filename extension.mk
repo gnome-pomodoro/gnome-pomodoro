0d70be552f31c962487bd93c856ee64b30c0b9a2
Kamil Prusko <kamilprusko@gmail.com>
2012-11-16 22:55:29 +0100

    Fix removed extension.mk

--- extension.mk
+++
@@ -1,8 +0,0 @@
-# Include all pomodoro specific variables
-
-uuid = pomodoro@arun.codito.in
-topextensiondir = $(datadir)/gnome-shell/extensions
-extensiondir = $(topextensiondir)/$(uuid)
-
-gschemas_in = org.gnome.shell.extensions.pomodoro.gschema.xml.in
-gschemaname = org.gnome.shell.extensions.pomodoro
