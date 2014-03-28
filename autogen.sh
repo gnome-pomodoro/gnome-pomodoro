fb50ef9d3c4db7daba6c7aff1bc28f7cd4fcb19b
Kamil Prusko <kamilprusko@gmail.com>
2014-03-28 14:29:27 +0100

    Fix remote build

--- autogen.sh
+++ autogen.sh
@@ -4,20 +4,15 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="gnome-pomodoro"
-
 (test -f $srcdir/configure.ac) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+    echo -n "**Error**: Directory \"\'$srcdir\'\" does not look like the"
+    echo " top-level package directory"
     exit 1
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git (or from"
-    echo "your OS vendor's package manager)."
+    echo "You need to install gnome-common!"
     exit 1
 }
 
-git submodule update --init --recursive
-
-REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9 GNOME_DATADIR="$gnome_datadir" . gnome-autogen.sh
