bc7c8a5a417355b4fe781260a828124818a2dbca
Kamil Prusko <kamilprusko@gmail.com>
2021-07-04 12:45:58 +0200

    Make meson-post-install.sh compatible with other shells
    Use of [[ ]] made it bash only.

--- meson-post-install.sh
+++ meson-post-install.sh
@@ -5,13 +5,13 @@ datadir="${prefix}/$1"
 
 # Distro packagers define DESTDIR and they don't
 # want/need us to do the below
-if [[ -z $DESTDIR ]]; then
+if [ -z "${DESTDIR}" ]; then
     echo "Compiling GSchema..."
-    glib-compile-schemas $datadir"/glib-2.0/schemas"
+    glib-compile-schemas "${datadir}/glib-2.0/schemas"
 
     echo "Updating icon cache..."
-    gtk-update-icon-cache -f -t $datadir"/icons/hicolor"
+    gtk-update-icon-cache -f -t "${datadir}/icons/hicolor"
 
     echo "Updating desktop database..."
-    update-desktop-database -q $datadir"/applications"
+    update-desktop-database -q "${datadir}/applications"
 fi
